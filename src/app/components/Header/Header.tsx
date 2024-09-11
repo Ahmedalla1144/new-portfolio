611a439df3dfb039e3d62e3d8ba7a7001fda75b9
Ahmed alaam <ahmed.alla56756@gmail.com>
2024-09-12 00:51:55 +0300

    go

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
         <DiCssdeck className="lg:text-5xl text-4xl" />
         <h1>Portfolio</h1>
       </Link>
-      <div className="flex gap-4 text-3xl  lg:gap-12 lg:text-4xl leading-8 text-[#ffffffbf] order-3 md:order-2 m-auto md:pt-0 pt-5 px-0 md:px-0 ss:px-32 xs:px-56 ">
+      <div className="flex gap-4 text-2xl  lg:gap-12 lg:text-4xl leading-8 text-[#ffffffbf] order-3 md:order-2 m-auto md:pt-0 pt-5 px-0 md:px-0 ss:px-32 xs:px-56 ">
         <Link className={styles?.link} href="projects">
           Projects
         </Link>
