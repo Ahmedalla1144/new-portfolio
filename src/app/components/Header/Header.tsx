5799fdd041399f0f946de7ad3526b525b086df61
Ahmed alaam <ahmed.alla56756@gmail.com>
2024-09-11 23:47:35 +0300

    go

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -5,15 +5,15 @@ import Social from "../Social/Social";
 
 export default function Header() {
   return (
-    <header className="flex flex-wrap justify-between items-center px-5 py-3 lg:px-16 lg:py-10 text-2xl ss:text-3xl lg:text-4xl -tracking-[-0.2rem]">
+    <header className="flex flex-wrap justify-between items-center px-5 py-5 lg:px-16 lg:py-10 text-3xl lg:text-4xl -tracking-[-0.2rem]">
       <Link
         href="/"
         className="flex justify-center items-center hover:cursor-pointer order-1"
       >
-        <DiCssdeck className="lg:text-5xl ss:text-4xl text-3xl" />
+        <DiCssdeck className="lg:text-5xl text-4xl" />
         <h1>Portfolio</h1>
       </Link>
-      <div className="flex gap-4 ss:text-2xl lg:gap-12 lg:text-3xl text-xl leading-8 text-[#ffffffbf] order-3 md:order-2 m-auto md:pt-0 pt-5 px-0 md:px-0 ss:px-32 xs:px-56 ">
+      <div className="flex gap-4 text-2xl  lg:gap-12 lg:text-4xl leading-8 text-[#ffffffbf] order-3 md:order-2 m-auto md:pt-0 pt-5 px-0 md:px-0 ss:px-32 xs:px-56 ">
         <Link className={styles?.link} href="projects">
           Projects
         </Link>
@@ -24,7 +24,7 @@ export default function Header() {
           About
         </Link>
       </div>
-      <div className="flex lg:gap-5 lg:text-5xl ss:text-4xl text-3xl order-2 md:order-3 justify-center items-center">
+      <div className="flex lg:gap-5 lg:text-5xl text-4xl order-2 md:order-3 justify-center items-center">
         <Social />
       </div>
     </header>
