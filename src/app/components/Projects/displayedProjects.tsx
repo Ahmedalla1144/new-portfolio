da56efbea3887abeb8912bb867de38b8fe04df1a
Ahmed alaam <ahmed.alla56756@gmail.com>
2024-12-25 14:43:00 +0200

    go

--- src/app/components/Projects/displayedProjects.tsx
+++ src/app/components/Projects/displayedProjects.tsx
@@ -16,10 +16,11 @@ export default function DisplayedProjects({ projects }: DisplayedProjects) {
     setTimeout(() => {
       setDisplayedProjects(displayedProjects + 2);
       setIsAnimating(false);
-    }, 300);
+    }, 100);
   };
   return (
     <Section title="Projects">
+      Ahmed
       <div className="grid grid-col-1 lg:grid-cols-2 place-items-center gap-20 py-10">
         {projects.slice(0, displayedProjects).map((project) => (
           <div
