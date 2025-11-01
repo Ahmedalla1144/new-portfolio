3ca274869ac40c568d895fc236a62311d6981bb3
Ahmed Alaa <ahmed.alla56756@gmail.com>
2025-11-01 15:52:48 +0200

    fixing deploy for netlify

--- src/app/components/Projects/getProjects.tsx
+++ src/app/components/Projects/getProjects.tsx
@@ -2,7 +2,8 @@ export default async function getProjects() {
   // Fetch data from API
   const res = await fetch(
     "https://www.ahmed.alaa1144.apis.mrbotusa.com/api/projects/1",
-    { next: { revalidate: 10 } }
+    // { next: { revalidate: 10 } }
+    { cache: "no-store" }
   );
   if (!res.ok) {
     throw new Error("Failed to fetch projects");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import Technologies from "./components/Technologies/Technologies";
 
-export const dynamic = 'force-dynamic';
-
 export default function Home() {
   return (
     <>
