3d094c85d2f840c45fce435de23d225d63eab499
Ahmed Alaa <ahmed.alla56756@gmail.com>
2025-11-01 15:56:31 +0200

    fixing deploy for netlify

--- src/app/components/Projects/getProjects.tsx
+++ src/app/components/Projects/getProjects.tsx
@@ -2,7 +2,7 @@ export default async function getProjects() {
   // Fetch data from API
   const res = await fetch(
     "https://www.ahmed.alaa1144.apis.mrbotusa.com/api/projects/1/",
-    // { next: { revalidate: 10 } }
+    { next: { revalidate: 10 } }
   );
   if (!res.ok) {
     throw new Error("Failed to fetch projects");
