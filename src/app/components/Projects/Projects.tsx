846db9db70fc89bc7d9439d8038990778c9afeba
Ahmed Alaa <ahmed.alla56756@gmail.com>
2025-11-01 16:11:52 +0200

    fixing deploy for netlify

--- src/app/components/Projects/Projects.tsx
+++ src/app/components/Projects/Projects.tsx
@@ -1,8 +1,6 @@
 import getProjects from "./getProjects";
 import DisplayedProjects from "./displayedProjects";
 
-export const dynamic = 'force-dynamic';
-
 export default async function Projects() {
   const projects: Project[] = await getProjects();
   return <DisplayedProjects projects={projects} />;
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Projects from '../components/Projects/Projects'
 
+export const dynamic = 'force-dynamic';
+
 export default function projects() {
   return (
     <Projects />
